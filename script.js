bdca2343680b752debb86f7b6e405d841343952e
HernanRPereira <hernxnpereyra@gmail.com>
2026-03-07 10:59:42 -0500

    Format and found

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function ordenarPorFecha() {
-    const contenedor = document.getElementById("informes");
-    const dias = Array.from(contenedor.getElementsByClassName("dia"));
-  
-    dias.sort((a, b) => {
-      const fechaA = new Date(a.dataset.fecha);
-      const fechaB = new Date(b.dataset.fecha);
-      return fechaB - fechaA;
-    });
-  
-    dias.forEach(dia => contenedor.appendChild(dia));
-  }
+  const contenedor = document.getElementById("informes");
+  const dias = Array.from(contenedor.getElementsByClassName("dia"));
+
+  dias.sort((a, b) => {
+    const fechaA = new Date(a.dataset.fecha);
+    const fechaB = new Date(b.dataset.fecha);
+    return fechaB - fechaA;
+  });
+
+  dias.forEach(dia => contenedor.appendChild(dia));
+}
